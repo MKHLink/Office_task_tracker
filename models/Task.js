4e47642e12e9d3690810623a5af850eb4aa02ce8
MKHLink <[likhonhasankomol@gmail.com]>
2022-11-05 16:40:37 -0400

    added task table

---
+++ models/Task.js
@@ -0,0 +1,50 @@
+const {Model, DataTypes} = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Task extends Model {}
+
+Task.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement:true
+        },
+
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+
+        deadline: {
+            type: DataTypes.DATE,
+            allowNull: false
+        },
+        
+        manager_id : {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'manager',
+                key: 'id'
+            }
+        },
+
+        employee_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'employee',
+                key: 'id'
+            }
+        }
+    },
+
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'task'
+    }
+);
+
+module.exports = Task;
