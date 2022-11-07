10cded971b74c981c1cbe6a9e973acf785adb926
MKHLink <[likhonhasankomol@gmail.com]>
2022-11-07 17:06:39 -0500

    able to display tasks on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,29 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const {Manager, Employee, Task} = require('../models');
 
 router.get('/',(req,res)=>{
-    res.render('homepage');
+    Task.findAll({
+        attributes: ['id', 'title', 'deadline', 'created_at'],
+        include: [
+          {
+              model: Manager,
+              attributes: ['first_name', 'last_name']
+          }, 
+          {
+              model: Employee,
+              attributes: ['first_name', 'last_name']
+          }
+        ]
+      })
+        .then(dbTaskData => {
+            const tasks = dbTaskData.map(task => task.get({plain:true}));
+            res.render('homepage',{tasks});
+        })
+        .catch(err => {
+          console.log(err);
+          res.status(500).json(err);
+        });
 });
 
 module.exports = router;
