9591b121ac57e698fe37465f596253af4df7bd66
MKHLink <[likhonhasankomol@gmail.com]>
2022-11-10 14:36:00 -0500

    fixed up merge mess

--- controllers/index.js
+++ controllers/index.js
@@ -3,12 +3,12 @@ const router = require('express').Router();
 const homeRoutes = require('./home-routes');
 const apiRoutes = require('./api');
 const dashboardRoutes = require('./dashboard-routes.js');
-const employeeDashRoutes = require('./employee-dash_routes.js');
+
 
 router.use('/api',apiRoutes);
 router.use('/',homeRoutes);
 router.use('/dashboard', dashboardRoutes);
-router.use('/employee-dashboard',employeeDashRoutes);
+
 
 router.use((req,res)=>{
     res.status(404).end();
